6639db4f086d628c7d02088733c484c006294eef
Brian Dean Jennings <brian@bdjnews.com>
2017-12-18 00:58:59 +1300

    Code cleanup

--- AdventOfCode-JS/Day17.ts
+++ AdventOfCode-JS/Day17.ts
@@ -1,13 +1,13 @@
-﻿// http://adventofcode.com/2017/day/16
+﻿// http://adventofcode.com/2017/day/17
 
 export var title = "Day 17";
 
 export function solve(input) {
-    solve17A(input);
-    solve17B(input);
+    solveA(input);
+    solveB(input);
 }
 
-function solve17A(input) {
+function solveA(input) {
     let pos = 0;
     const buffer = [0];
     for (let i = 1; i <= 2017; i++) {
@@ -19,7 +19,7 @@ function solve17A(input) {
     console.log(`Value after 2017: ${buffer[pos + 1]}`);
 }
 
-function solve17B(input) {
+function solveB(input) {
     let pos = 0;
     let length = 1;
     let targetValue = 0;
@@ -32,7 +32,6 @@ function solve17B(input) {
         if (pos == 1) {
             targetValue = i;
         }
-        //if (i % 1e6 == 0) console.log(`${i / 1e6} million...`);
     }
 
     console.log(`Value after 5,000,000: ${targetValue}`);
